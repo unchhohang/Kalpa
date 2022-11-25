9af539728421176ef1fed0a66d0eae9b498acc0e
unchhohang <unchhohang@gmail.com>
2022-11-25 12:06:27 +0545

    Custom nav done

--- src/component/NavSideBar.jsx
+++ src/component/NavSideBar.jsx
@@ -2,15 +2,71 @@
  * Side Bar
  */
 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/NavSideBar.css";
+import logo from "../resource/logo.jpeg";
+
 export default function NavSideBar() {
+
+  //Selected title
+  const [sTitle, setSTitle] = useState('Billing');
+
+  // Function to select navigation title
+  function navTitle(title){
+    setSTitle(title);
+  }
   return (
-    <div className="navBar">
-      <div>
-        <h1>Nav side bar</h1>
+    <>
+      <div className="navBar">
+        <div>
+          <img
+            src={logo}
+            width="200"
+            alt="Logo"
+            style={{ borderRadius: "200px" }}
+          />
+        </div>
+        <ul>
+          {data.map((item, index) => {
+            return (
+              <Link to={item.path} className="nav-link">
+                <li className={sTitle === item.title ? "selected-li-navBar": "li-navBar"} 
+                onClick={()=>navTitle(item.title)}
+                >
+                  <span>
+                    <h3>{item.title}</h3>
+                  </span>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
       </div>
-      <div>
-        <h1>Nav side bar2</h1>
-      </div>
-    </div>
+    </>
   );
 }
+
+/**
+ * Data for nav bar Navigation
+ *
+ */
+
+const data = [
+  {
+    title: "Billing",
+    path: "/",
+  },
+  {
+    title: "Products",
+    path: "/products",
+  },
+  {
+    title: "Stock",
+    path: "/stock",
+  },
+  {
+    title: "Report",
+    path: "/report",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Billing from "./component/Billing";
 import NavSideBar from "./component/NavSideBar";
 import Products from "./component/Products";
+import { CssBaseline } from "@mui/material";
+import { Container } from "@mui/system";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    {/* <Container> */}
+      <App />
+    {/* </Container> */}
   </React.StrictMode>
 );
