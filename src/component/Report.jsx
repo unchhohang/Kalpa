54297cd168241bb6bf0dde7a5f806f6765f50cb8
Indra Bahadur <indra.oli@nicnepal.org>
2022-12-06 16:25:01 +0545

    billing form

--- src/component/Report.jsx
+++ src/component/Report.jsx
@@ -2,7 +2,17 @@
  * Report screen component
  */
 
-import { Box, Breadcrumbs, Chip, Divider, Link, Switch, Tab, Tabs, Typography } from "@mui/material";
+import {
+  Box,
+  Breadcrumbs,
+  Chip,
+  Divider,
+  Link,
+  Switch,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
 import moment from "moment/moment";
 import React, { useState } from "react";
 import "../css/main.css";
@@ -11,11 +21,11 @@ import DateReport from "./DateReport";
 
 // import Box from '@mui/material/Box';
 // import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import HomeIcon from "@mui/icons-material/Home";
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import AssessmentIcon from "@mui/icons-material/Assessment";
 
 export default function Report() {
   /**
@@ -23,64 +33,61 @@ export default function Report() {
    * <DateReport/>
    */
 
+  const [value, setValue] = React.useState("1");
 
-   const [value, setValue] = React.useState('1');
-
-   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-     setValue(newValue);
-   };
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
-    // <div className="container">
-    //   <div className="toggle-report">
-    //     <Switch
-    //       checked={tog}
-    //       onChange={() => {
-    //         setTog(!tog);
-    //       }}
-    //       name="tog"
-    //       color="success"
-    //     />
-    //   </div>
-    //   {tog ? <DateReport data={fake} /> : <DailyReport data={fake}/>}
-    // </div>
-<>
-    <Breadcrumbs>
-        <Link 
-          underline="hover" 
-          color="inherit" 
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="/">
-        <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          EBS
-        </Link>
-        <Link 
-          underline="hover" 
-          color="inherit"
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="/report">
-        <AssessmentIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Report
-        </Link>
-        <Typography color="text.primary">Manage Report</Typography>
-      </Breadcrumbs>
-      {/* <Divider >
+    <>
+      <div className="container">
+        <Breadcrumbs>
+          <Link
+            underline="hover"
+            color="inherit"
+            sx={{ display: "flex", alignItems: "center" }}
+            href="/"
+          >
+            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            EBS
+          </Link>
+          <Link
+            underline="hover"
+            color="inherit"
+            sx={{ display: "flex", alignItems: "center" }}
+            href="/report"
+          >
+            <AssessmentIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            Report
+          </Link>
+          <Typography color="text.primary">Manage Report</Typography>
+        </Breadcrumbs>
+        {/* <Divider >
         <Chip label="Report" color="success"/>
       </Divider> */}
 
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value} >
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} centered aria-label="lab API tabs example">
-          <Tab label="Daily report" value="1" />
-            <Tab label="Date wise report" value="2" />
-            
-          </TabList>
+        <Box sx={{ width: "100%", typography: "body1" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                centered
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Daily report" value="1" />
+                <Tab label="Date wise report" value="2" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <DailyReport data={fake} />
+            </TabPanel>
+            <TabPanel value="2">
+              <DateReport data={fake} />
+            </TabPanel>
+          </TabContext>
         </Box>
-        <TabPanel value="1"><DailyReport data={fake}/></TabPanel>
-        <TabPanel value="2"><DateReport data={fake} /></TabPanel>
-      </TabContext>
-    </Box>
+      </div>
     </>
   );
 }
