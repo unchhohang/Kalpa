9fa8220517a526450f80b8a9406816a4bc461a9f
unchhohang <unchhohang@gmail.com>
2022-12-06 17:10:48 +0545

    report section 90% complete.

--- src/component/DailyReport.jsx
+++ src/component/DailyReport.jsx
@@ -16,11 +16,14 @@ import {
 import ReportTable from "./ReportTable";
 
 export default function DailyReport(props) {
+  let bills = props.bills;
+  let stocks = props.stocks;
+
   return (
     <div>
-      <div className="heads">
-        {/* <h1>Daily Report</h1> */}
-        {/* <span> */}
+        <div className="heads">
+          {/* <h1>Daily Report</h1> */}
+          {/* <span> */}
           <Button
             className="pdf-btn"
             variant="contained"
@@ -29,131 +32,51 @@ export default function DailyReport(props) {
           >
             Export to PDF
           </Button>
-        {/* </span> */}
-      </div>
-      <div>
-        <ReportTable data={props.data} />
-      </div>
-      <div className="report-stock-container">
-        <h1>Stock Remaining</h1>
-        <div>
-          <TableContainer className="report-action-table">
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead sx={{ backgroundColor: "#D9D9D9" }}>
-                <TableRow>
-                  <TableCell>
-                    <h3>Stock Name</h3>
-                  </TableCell>
-                  <TableCell>
-                    <h3>Quantity</h3>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow
-                  // key={}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    <Typography>Mango Pickel</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography>2</Typography>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
         </div>
-      </div>
+        <div>
+          {
+            // Case for no bills
+            bills.length !== 0 && <ReportTable bills={bills} />
+          }
+        </div>
+        <div className="report-stock-container">
+          <h1>Stock Remaining</h1>
+          <div>
+            <TableContainer className="report-action-table">
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead sx={{ backgroundColor: "#D9D9D9" }}>
+                  <TableRow>
+                    <TableCell>
+                      <h3>Stock Name</h3>
+                    </TableCell>
+                    <TableCell>
+                      <h3>Quantity</h3>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {stocks.map((x, index) => {
+                    return (
+                      <TableRow
+                        // key={}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell>
+                          <Typography>{x.productName}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{x.quantity}</Typography>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </div>
     </div>
   );
 }
--- src/component/Report.jsx
+++ src/component/Report.jsx
@@ -2,20 +2,31 @@
  * Report screen component
  */
 
-import { Box, Breadcrumbs, Chip, Divider, Link, Switch, Tab, Tabs, Typography } from "@mui/material";
+import {
+  Box,
+  Breadcrumbs,
+  Chip,
+  Divider,
+  Link,
+  Switch,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
 import moment from "moment/moment";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/main.css";
 import DailyReport from "./DailyReport";
 import DateReport from "./DateReport";
 
 // import Box from '@mui/material/Box';
 // import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import HomeIcon from "@mui/icons-material/Home";
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import axios from "axios";
 
 export default function Report() {
   /**
@@ -23,136 +34,94 @@ export default function Report() {
    * <DateReport/>
    */
 
+  const [value, setValue] = useState("1");
+  const [bills, setBills] = useState([]);
+  const [productStocks, setProductStocks] = useState([]);
 
-   const [value, setValue] = React.useState('1');
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-     setValue(newValue);
-   };
+  // Using use effect to render bills and productStocks
+  // Based on current date
+
+  useEffect(() => {
+    // get current date to set current dates bills
+
+    let dateArray = moment().format().split("T");
+    let currentDate = dateArray[0];
+
+    console.log(`useEffect in effect...`);
+    console.log(bills);
+    getBills(currentDate);
+    getProductStocks();
+  }, [value]);
+
+  // Retrive bills by date
+  function getBills(date) {
+    axios
+      .get("/report", { params: { date: date } })
+      .then((data) => {
+        console.log("api report");
+        console.log(data.data);
+        setBills(data.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function getProductStocks() {
+    axios
+      .get("/productStock")
+      .then((data) => setProductStocks(data.data))
+      .catch((err) => console.log(err));
+  }
 
   return (
-    // <div className="container">
-    //   <div className="toggle-report">
-    //     <Switch
-    //       checked={tog}
-    //       onChange={() => {
-    //         setTog(!tog);
-    //       }}
-    //       name="tog"
-    //       color="success"
-    //     />
-    //   </div>
-    //   {tog ? <DateReport data={fake} /> : <DailyReport data={fake}/>}
-    // </div>
-<>
-    <Breadcrumbs>
-        <Link 
-          underline="hover" 
-          color="inherit" 
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="/">
-        <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          EBS
-        </Link>
-        <Link 
-          underline="hover" 
-          color="inherit"
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="/report">
-        <AssessmentIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Report
-        </Link>
-        <Typography color="text.primary">Manage Report</Typography>
-      </Breadcrumbs>
-      {/* <Divider >
-        <Chip label="Report" color="success"/>
-      </Divider> */}
+    <>
+      <div>
+        <Breadcrumbs>
+          <Link
+            underline="hover"
+            color="inherit"
+            sx={{ display: "flex", alignItems: "center" }}
+            href="/"
+          >
+            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            EBS
+          </Link>
+          <Link
+            underline="hover"
+            color="inherit"
+            sx={{ display: "flex", alignItems: "center" }}
+            href="/report"
+          >
+            <AssessmentIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            Report
+          </Link>
+          <Typography color="text.primary">Manage Report</Typography>
+        </Breadcrumbs>
 
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value} >
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} centered aria-label="lab API tabs example">
-          <Tab label="Daily report" value="1" />
-            <Tab label="Date wise report" value="2" />
-            
-          </TabList>
+        <Box sx={{ width: "100%", typography: "body1" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                centered
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Daily report" value="1" />
+                <Tab label="Date wise report" value="2" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <DailyReport bills={bills} stocks={productStocks} />
+            </TabPanel>
+            <TabPanel value="2">
+              <DateReport bills={bills} getBills={getBills} />
+            </TabPanel>
+          </TabContext>
         </Box>
-        <TabPanel value="1"><DailyReport data={fake}/></TabPanel>
-        <TabPanel value="2"><DateReport data={fake} /></TabPanel>
-      </TabContext>
-    </Box>
+      </div>
     </>
   );
 }
-
-// Fakes are creating Fakes
-const fake = [
-  {
-    billNo: 22,
-    date: moment().format().toString(),
-    orders: [
-      {
-        productName: "satu",
-        price: 100,
-        quantity: 20,
-        amount: 2000,
-      },
-      {
-        productName: "Orange",
-        price: 15,
-        quantity: 2,
-        amount: 30,
-      },
-      {
-        productName: "coke",
-        price: 70,
-        quantity: 2,
-        amount: 140,
-      },
-    ],
-    subAmount: 2170,
-    customerName: "Simon",
-    customerAddress: "Kuleshor",
-    contactNo: 9856560000,
-    printedBy: "INndra",
-    paymentMode: "Cash",
-    discount: 50,
-    taxableAmount: 500,
-    vatAmount: 65,
-    totalAmount: 5656,
-  },
-  {
-    billNo: 2,
-    date: moment().format(),
-    orders: [
-      {
-        productName: "Lapsi",
-        price: 50,
-        quantity: 11,
-        amount: 555,
-      },
-      {
-        productName: "Lacto",
-        price: 10,
-        quantity: 5,
-        amount: 50,
-      },
-      {
-        productName: "aalu",
-        price: 100,
-        quantity: 1,
-        amount: 150,
-      },
-    ],
-    subAmount: 255,
-    customerName: "Dio",
-    customerAddress: "Balkhu",
-    contactNo: 9856550000,
-    printedBy: "Uttam",
-    paymentMode: "QR",
-    discount: 50,
-    taxableAmount: 500,
-    vatAmount: 65,
-    totalAmount: 568,
-  },
-];
