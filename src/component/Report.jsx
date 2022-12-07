7b84a7a9e24f6c990ef5c2fd1faafd466ecdf708
[Simon Shrestha] <[simonstha99@gmail.com]>
2022-12-07 13:50:14 +0545

    merge conflict + all popup finished

--- src/component/Report.jsx
+++ src/component/Report.jsx
@@ -131,3 +131,74 @@ export default function Report() {
     </>
   );
 }
+
+const fake = [
+  {
+    billNo: 22,
+    date: moment().format().toString(),
+    orders: [
+      {
+        productName: "satu",
+        price: 100,
+        quantity: 20,
+        amount: 2000,
+      },
+      {
+        productName: "Orange",
+        price: 15,
+        quantity: 2,
+        amount: 30,
+      },
+      {
+        productName: "coke",
+        price: 70,
+        quantity: 2,
+        amount: 140,
+      },
+    ],
+    subAmount: 2170,
+    customerName: "Simon",
+    customerAddress: "Kuleshor",
+    contactNo: 9856560000,
+    printedBy: "INndra",
+    paymentMode: "Cash",
+    discount: 50,
+    taxableAmount: 500,
+    vatAmount: 65,
+    totalAmount: 5656,
+  },
+  {
+    billNo: 2,
+    date: moment().format(),
+    orders: [
+      {
+        productName: "Lapsi",
+        price: 50,
+        quantity: 11,
+        amount: 555,
+      },
+      {
+        productName: "Lacto",
+        price: 10,
+        quantity: 5,
+        amount: 50,
+      },
+      {
+        productName: "aalu",
+        price: 100,
+        quantity: 1,
+        amount: 150,
+      },
+    ],
+    subAmount: 255,
+    customerName: "Dio",
+    customerAddress: "Balkhu",
+    contactNo: 9856550000,
+    printedBy: "Uttam",
+    paymentMode: "QR",
+    discount: 50,
+    taxableAmount: 500,
+    vatAmount: 65,
+    totalAmount: 568,
+  },
+];
